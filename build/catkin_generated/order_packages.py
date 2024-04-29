71622fcdecf023f5cad4057d042e1adccafc0ad7
3mi1ian0 <A01706083@tec.mx>
2024-04-29 12:50:51 -0600

    Creacion del ws y pkg

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/emimendoza/quetzal_coders_m3/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/emimendoza/quetzal_coders_m3/devel;/opt/ros/noetic'.split(';') if '/home/emimendoza/quetzal_coders_m3/devel;/opt/ros/noetic' != '' else []
---
+++ build/modulo_3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/emimendoza/quetzal_coders_m3/src/modulo_3
+# Build directory: /home/emimendoza/quetzal_coders_m3/build/modulo_3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
